8ac63e409b5306b05b7c9e8a5513b7dff2ddc75d
junfenglau <wongjuanaa@gmail.com>
2024-05-20 04:58:08 -0400

    TODO: merge w/discuss, aichat, auth, signup, login/out

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import "./App.css";
+import HomePage from "./HomePage";
+import CreatePage from "./CreatePage";
+import Discuss from "./Discuss";
+import SearchPage from "./SearchPage";
+import ManagePage from "./ManagePage";
+import PostView from './PostView';
+import PostComments from './PostComments';
+import AiChat from './AiChat';
+import { AuthProvider } from './contexts/AuthContext';
+import UserLogin from './UserLogin';
+import UserSignup from './UserSignup';
+import UserPage from './UserPage';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+// requires user login to access
+function ProtectedRoutes() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Switch>
+      <Route path="/create" component={CreatePage} />
+      <Route path="/discuss" component={Discuss} />
+      <Route path="/manage" component={ManagePage} />
+      <Route path="/id/:post_id/Comments" component={PostComments} />
+      <Route path="/userpage" component={UserPage} />
+    </Switch>
+  );
 }
 
-export default App
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <Router> {/* Wrap your components with Router */}
+        <div className="App">
+          <div className="root">
+            <Switch> {/* Use Switch to render only one route */}
+              <AuthProvider>
+                <ProtectedRoutes />
+              </AuthProvider>
+              <Route path="/" exact component={HomePage} />
+              <Route path="/search" component={SearchPage} />
+              <Route path="/id/:post_id" component={PostView} />
+              <Route path="/id/:post_id/Comments" component={PostComments} />
+              <Route path="/signup" component={UserSignup} />
+              <Route path="/login" component={UserLogin} />
+              <Route path="/AiChat" component={AiChat} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+  </React.StrictMode>
+);
